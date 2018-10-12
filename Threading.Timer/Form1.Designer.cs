8ce4b4a1a2a2f096747cd426d5ab0673ea262e26
lidehua <lidehuawork@163.com>
2018-10-12 17:16:04 +0800

    增加定时器工具并附上简单的数据库操作

---
+++ Threading.Timer/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Threading.Timer
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_Count = new System.Windows.Forms.TextBox();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_Count
+            // 
+            this.txt_Count.Location = new System.Drawing.Point(92, 26);
+            this.txt_Count.Name = "txt_Count";
+            this.txt_Count.Size = new System.Drawing.Size(139, 21);
+            this.txt_Count.TabIndex = 7;
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Location = new System.Drawing.Point(156, 98);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_Stop.TabIndex = 6;
+            this.btn_Stop.Text = "停止计时";
+            this.btn_Stop.UseVisualStyleBackColor = true;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(28, 98);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 5;
+            this.btn_Start.Text = "开始计时";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "当前数值：";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(258, 156);
+            this.Controls.Add(this.txt_Count);
+            this.Controls.Add(this.btn_Stop);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Threading.Timer";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Count;
+        private System.Windows.Forms.Button btn_Stop;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Threading.Timer/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using CC.Public;
+using CC.Framework.DAL;
+using CC.DataAccess.Extend;
+
+namespace Threading.Timer
+{
+    public partial class FrmMain : Form
+    {
+        //定义全局变量
+        public int currentCount = 0;
+        //定义Timer类
+        System.Threading.Timer threadTimer;
+        //定义委托
+        public delegate void SetControlValue(object value);
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            InitTimer();
+        }
+
+        /// <summary>
+        /// 初始化Timer类
+        /// </summary>
+        private void InitTimer()
+        {
+            threadTimer = new System.Threading.Timer(new TimerCallback(TimerUp), null, Timeout.Infinite, 1000);
+        }
+
+        /// <summary>
+        /// 定时到点执行的事件
+        /// </summary>
+        /// <param name="value"></param>
+        private void TimerUp(object value)
+        {
+            currentCount += 1;
+            ViewData();
+            this.Invoke(new SetControlValue(SetTextBoxValue), currentCount);
+        }
+
+        /// <summary>
+        /// 给文本框赋值
+        /// </summary>
+        /// <param name="value"></param>
+        private void SetTextBoxValue(object value)
+        {
+            this.txt_Count.Text = value.ToString();
+        }
+
+        /// <summary>
+        /// 开始
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            //立即开始计时，时间间隔1000毫秒
+            threadTimer.Change(0, 3000);
+        }
+
+        /// <summary>
+        /// 停止
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Stop_Click(object sender, EventArgs e)
+        {
+            //停止计时
+            threadTimer.Change(Timeout.Infinite, 3000);
+        }
+        public static void ViewData()
+        {
+            HashObjectList datasourcecombase =
+                new DALBase("erp").GetDataList(HashObject.CreateWith("profileid", 10004621), "select * from bas_account where profileid=10004621;", SqlType.CmdText);
+            //Console.WriteLine(datasourcecombase.Count());
+        }
+    }
+}

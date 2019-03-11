3b9703d520c1088b90bd567a10e7b512d2bf3daa
lidehua <lidehuawork@163.com>
2019-03-11 16:00:48 +0800

    webapi问题处理

--- Super.WebApi/Controllers/ProductsController.cs
+++ Super.WebApi/Controllers/ProductsController.cs
@@ -3,9 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
 
 namespace Super.WebApi.Controllers
 {
@@ -18,13 +16,13 @@ namespace Super.WebApi.Controllers
             new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
             new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
          };
-        [System.Web.Http.Authorize]
-        [System.Web.Http.Route("")]
+        [Authorize]
+        [Route("")]
         public IEnumerable<Product> GetAllProducts()
         {
             return products;
         }
-        [System.Web.Http.Authorize]
+        [Authorize]
         public Product GetProductById(int id)
         {
             var product = products.FirstOrDefault((p) => p.Id == id);

ba25a697575b3af985d652ac6869d4be00701176
lidehua <lidehuawork@163.com>
2019-03-11 17:26:20 +0800

    添加学生类接口

---
+++ Super.WebApi/Controllers/StudentsController.cs
@@ -0,0 +1,36 @@
+﻿using Super.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace Super.WebApi.Controllers
+{
+    [System.Web.Http.RoutePrefix("api/Students")]
+    public class StudentsController : ApiController
+    {
+        Student[] students = new Student[]
+         {
+            new Student { Id = 1, Name = "TomatoSoup", Age = 18 },
+            new Student { Id = 2, Name = "Yoyo", Age = 19 },
+            new Student { Id = 3, Name = "Hammer", Age = 20 }
+         };
+        [Authorize]
+        [Route("")]
+        public IEnumerable<Student> GetAllStudents()
+        {
+            return students;
+        }
+        [Authorize]
+        public Student GetStudentById(int id)
+        {
+            var student = students.FirstOrDefault((p) => p.Id == id);
+            if (student == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return student;
+        }
+    }
+}
